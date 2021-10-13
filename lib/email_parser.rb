44812cd9c0ca6517df640ec5d3265106d3464bfe
Catherine Loesch <catherine.loesch@protonmail.com>
2021-10-13 13:56:54 +0100

    assignment completed

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,15 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+
+class EmailAddressParser
+    def initialize(email_adresses_string)
+        @email_adresses_string = email_adresses_string
+    end
+
+    def parse
+        @email_adresses_string.split(/, | |,/).uniq
+    end
+end
+
